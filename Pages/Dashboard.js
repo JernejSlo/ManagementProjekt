e7e9f6478eb5c15829e714935d31121f04b084c0
JernejSlo <jernejkoprivnikar.slo@gmail.com>
2024-05-18 13:08:42 +0200

    Changed dashboard

--- Pages/Dashboard.js
+++ Pages/Dashboard.js
@@ -1,14 +1,33 @@
 import {SafeAreaView} from "react-native-safe-area-context";
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
+import {useDispatch, useSelector} from "react-redux";
+import {selectUser, setUser} from "../Slices/navSlice";
 
 const Dashboard = () => {
 
+    let user = useSelector(selectUser)
+    let dispatch = useDispatch()
+
+    let newUser = {
+            name: "Matija Novak",
+            location: "Ljubljana, Slovenija",
+            img: require('../assets/UserIcons/KatjaIcon.png'),
+            rank: 33,
+        }
+    function setNewUser(){
+        dispatch(setUser(newUser))
+    }
 
     return (
         <SafeAreaView>
             <Text>
-                This is the dashboard
+                Welcome {user.name}!
             </Text>
+            <TouchableOpacity onPress={() => setNewUser()}>
+                <Text>
+                    Press me to change user
+                </Text>
+            </TouchableOpacity>
         </SafeAreaView>
     )
 }
