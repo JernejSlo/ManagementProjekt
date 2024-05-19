57824c6cf3e509bdfe938c49156847ea363cd0f7
rokciber <89211202@student.upr.si>
2024-05-19 20:09:09 +0200

    training PP upd

--- Pages/TrainingPlanPage.js
+++ Pages/TrainingPlanPage.js
@@ -9,12 +9,20 @@ import BarChart from "../Components/Large/Statistics/BarChart";
 import StatCard from "../Components/Small/Statistics/StatCard";
 import {useSelector} from "react-redux";
 import {selectUser} from "../Slices/navSlice";
+import {useNavigation} from "@react-navigation/native";
+
+
 
 export default function TrainingPlanPage({}){
     let user = useSelector(selectUser);
     return (
         <SafeAreaView style={{...styles.base_bg, alignItems: "center"}}>
             <NavigateAndTitle title={`${user.name}'s Training Plan`} />
+
+            <Text>Training plan</Text>
+
+
+
         </SafeAreaView>
     );
 }
